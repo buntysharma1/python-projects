2c4a272a8a4491f945f047d3b1eae971616c569e
Bunty Sharma <bunty.sharma@somaiya.edu>
2020-06-11 00:00:09 +0530

    hangman is modified

--- projects/hangman.py
+++ projects/hangman.py
@@ -2,63 +2,12 @@ import os
 import random
 import time
 
-Name=['bunty',
-      'sharma',
-      'aarush',
-      'ashok',
-      'joy',
-      'akash',
-      'pritam',
-      'deepshikha',
-      'vandana',
-      'baby',
-      'abhijeet',
-      'ekta']
-Fruits=['apple',
-        'banana',
-        'mango',
-        'pineapple',
-        'watermelon',
-        'papaya',
-        'pears',
-        'maskmelon']
-Vegetables=['ladzfinger',
-            'brinjal',
-            'broccoli',
-            'corn',
-            'lettuce',
-            'cucumber',
-            'beetroot',
-            'pumpkin']
-Superheroes=['superman',
-             'spiderman',
-             'batman',
-             'ironman',
-             'caption america',
-             'black widow',
-             'hulk',
-             'thor',
-             'antman',
-             'wolverin']
-Random_words=['hello',
-              'somaiya',
-              'kvains',
-              'backbanchers',
-              'football',
-              'alone',
-              'ronaldo',
-              'messi',
-              'father']
-Transports=['cars',
-            'auto',
-            'truces',
-            'suv',
-            'train',
-            'airoplane',
-            'ship',
-            'bycycle',
-            'bike']
-
+Name=['bunty','sharma','aarush','ashok','joy','akash','pritam','deepshikha','vandana','baby','abhijeet','ekta']
+Fruits=['apple','banana','mango','pineapple','watermelon','papaya','pears','maskmelon']
+Vegetables=['ladzfinger','brinjal','broccoli','corn','lettuce','cucumber','beetroot','pumpkin']
+Superheroes=['superman','spiderman','batman','ironman','caption america','black widow','hulk','thor','antman','wolverin']
+Random_words=['hello','somaiya','kvains','backbanchers','football','alone','ronaldo','messi','father']
+Transports=['cars','auto','truces','suv','train','airoplane','ship','bycycle','bike']
 def word_chooser( name ):
     os.system('cls')
     print("\n\n\t\tWelcome to Hangman game <( \"-\" )>")
@@ -85,44 +34,42 @@ def word_chooser( name ):
     else:
         word=randoms.get(choice)
         return word
-
+    
 def Hangman_logic(incomplete_word,word,name):
     wrong=0
     while wrong != 11 :
         os.system('cls')
-        person(wrong)
-        print(f'guess the word : {incomplete_word} ')
+        person(wrong,word)
+        print('guess the word: ' + "".join(incomplete_word))
+        print("You have " + str(10 - wrong) + " lifes")
         letter=str(input('Enter Letter : '))
         if letter in word:
-            for i in range(len(word)):
-                if word[i] == letter:
-                    x=list(incomplete_word)
-                    incomplete_word=""
-                    x[i]=letter
-                    for l in x:
-                        incomplete_word+=l
+            index=0
+            if letter in incomplete_word:
+                index=incomplete_word.index(letter)
+                index=word.find(letter,index+1)
+            else:
+                index=word.index(letter)
+            incomplete_word[index]=letter
         else:
             wrong+=1
             print('\n Its a Wrong letter \nTry Again...')
-        if incomplete_word == word:
+            print("You have " + str(10 - wrong) + " lifes")
+        if "".join(incomplete_word) == word:
             os.system('cls')
             print(f"\n\n\t\t\tGreat {name}, You Saved Him (\"-\") !")
-            person(11)
-            time.sleep(2)
+            person(11,word)
+            time.sleep(1)
             exit()
-
 def main():
     print("\n\n\t\t\tWelcome to Hangman Game <(\"_\")>")
     name = input("Enter Name : ")
     os.system('cls')
     word = word_chooser(name)
-    incomplete_word=""
-    for _ in range(len(word)):
-        incomplete_word+="-"
-    Hangman_logic(incomplete_word,word,name)
+    incomplete_word = list("-" * len(word))
+    Hangman_logic(incomplete_word,word, name)
 
-
-def person(n):
+def person(n,word):
     print("\n=========")
     if (n==1):
         print(" 0 ")
@@ -162,11 +109,14 @@ def person(n):
         print(" /|\\  ")
         print(" / \\ ")
         print("\nYOU MAKE HIM DIE ")
+        print(f"\n Word Was {word} ")
         time.sleep(2)
         exit()
     elif(n==11):
         print(" \\ 0 / ")
         print("   |  ")
         print("  / \\ ")
+        print(f"\n Word is {word} ")
     print("\n")
-main()
+if __name__=="__main__":
+    main()
